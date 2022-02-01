04ac911c8b7ca404397891745743ee4b46876481
rumesh <rumesh.m.s16@gmail.com>
2022-02-01 18:24:56 +0530

    added is busy

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 from load_model import Args, load_model, generate
 
 app = FastAPI()
+# The ML model takes a significant amount of time to generate results.
+# whether the model is generating right now or not is stored in `is_busy`
+is_busy = 0
 
 
 class RequestBody(BaseModel):
@@ -11,11 +14,18 @@ class RequestBody(BaseModel):
 
 @app.post('/generate')
 def generate_simp(body: RequestBody):
-    text = body.text
-    if Args.model_path == "":
-        load_model()
-    out = generate(text.split("\n"))
-    return {"simplification": out}
+    try:
+        text = body.text
+        if Args.model_path == "":
+            load_model()
+        global is_busy
+        is_busy = 1
+        out = generate(text.split("\n"))
+        is_busy = 0
+        return {"simplification": out}
+    except Exception as e:
+        is_busy = 0
+        return {"error": str(e)}, 500
 
 
 @app.get('/health')
@@ -23,6 +33,12 @@ def health():
     return {}
 
 
+@app.get('/busy')
+def busy_status():
+    return {"is_busy": is_busy}
+
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", debug=True, reload=True)
