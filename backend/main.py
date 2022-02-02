595d7a82bcf1ace5f2a3b94e2e6ddae94f80c1d6
rumesh <rumesh.m.s16@gmail.com>
2022-02-02 07:52:31 +0530

    added logs

--- backend/main.py
+++ backend/main.py
@@ -20,16 +20,16 @@ class RequestBody(BaseModel):
 def generate_simp(body: RequestBody):
     try:
         text = body.text
-        logger.debug("Received for /generate: "+str(text))
+        logger.debug("Received for /generate: " + str(text))
         if Args.model_path == "":
             load_model()
         global is_busy
         is_busy = 1
         input_sent = text.split("\n")
-        logger.debug("Length of input: "+str(len(input_sent)))
-        logger.debug("Input: "+str(input_sent))
+        logger.debug("Length of input: " + str(len(input_sent)))
+        logger.debug("Input: " + str(input_sent))
         out = generate(input_sent)
-        logger.info("Output from /generate: "+str(out))
+        logger.info("Output from /generate: " + str(out))
         is_busy = 0
         return {"simplification": out}
     except Exception as e:
@@ -50,7 +50,8 @@ def busy_status():
 
 if __name__ == "__main__":
     import uvicorn
+
     logger.info("Loading the model................")
     load_model()
     logger.info("Model Loaded to memory")
-    uvicorn.run("main:app", debug=True, reload=True)
+    uvicorn.run("main:app", debug=True, reload=True, log_config=None)
