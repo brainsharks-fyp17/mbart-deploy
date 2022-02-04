3449a48eb5f79f002a2df284398e432cb1fd3e35
rumesh <rumesh.m.s16@gmail.com>
2022-02-04 23:26:02 +0530

    docker added cache

--- backend/cache_model.py
+++ backend/cache_model.py
@@ -1,15 +1,11 @@
 import os
 from dotenv import load_dotenv
-import torch
 from transformers import MBartForConditionalGeneration, MBartTokenizer
 
 load_dotenv()
 model_path = os.getenv("MODEL_PATH", "Rumesh/txt-smp-mbart")
 
 print("Loading the model..............")
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = MBartForConditionalGeneration.from_pretrained(model_path)
-model.to(device)
 tokenizer = MBartTokenizer.from_pretrained(model_path)
 print("Loaded the model")
