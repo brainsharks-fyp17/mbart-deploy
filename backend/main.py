3845a471e3c962cfea94c05706bce7fb8faf57d7
rumesh <rumesh.m.s16@gmail.com>
2022-02-05 09:46:52 +0530

    new model changed the model to lead globally

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
-import logging.config
 import traceback
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from load_model import generate
+
+from transformers import MBartTokenizer, MBartForConditionalGeneration
+import torch
+from dotenv import load_dotenv
+import logging.config
+import os
 from timeit import default_timer as timer
 
-# logging.config.fileConfig('logging.conf')
-# logger = logging.getLogger('frontend')
-
+load_dotenv()
 app = FastAPI()
 # The ML model takes a significant amount of time to generate results.
 # whether the model is generating right now or not is stored in `is_busy`
@@ -16,12 +18,61 @@ is_busy = 0
 logger = logging.getLogger("uvicorn.access")
 
 
+class Args:
+    model_path = os.getenv("MODEL_PATH", "Rumesh/mbart-si-simp")
+    max_length = os.getenv("MAX_LENGTH", "700")
+    num_beams = os.getenv("NUM_BEAMS", "5")
+    task = os.getenv("TASK", "com-sim")
+
+
+# Load the model globally
+model = None
+tokenizer = None
+device = None
+
+
 @app.on_event("startup")
-async def startup_event():
-    logger = logging.getLogger("uvicorn.access")
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-    logger.addHandler(handler)
+def startup_event():
+    global model
+    global device
+    global tokenizer
+    print("Loading the model..............")
+    start_timer = timer()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = MBartForConditionalGeneration.from_pretrained(Args.model_path)
+    model.to(device)
+    tokenizer = MBartTokenizer.from_pretrained(Args.model_path)
+    end_timer = timer()
+    print("Loaded the model")
+    print("Time taken to load the model: " + str(round(end_timer - start_timer, 4)) + " s")
+    del start_timer
+    del end_timer
+
+
+def generate(source_sentences: list):
+    global tokenizer
+    global model
+    global device
+    generated = ""
+    # if multiple sentences are in the input sentence, multiple outputs will be generated
+    for line in source_sentences:
+        # Attach task prefix.
+        line = Args.task + ": " + line
+
+        inputs = tokenizer(line, max_length=700, return_tensors="pt", padding=True).to(device)
+        summary_ids = model.generate(inputs["input_ids"], num_beams=int(Args.num_beams),
+                                     max_length=int(Args.max_length)).to(device)
+        out = tokenizer.batch_decode(summary_ids, skip_special_tokens=True,
+                                     clean_up_tokenization_spaces=False)
+
+        # Remove pad and eos tokens.
+        out = out[0]
+        out = out.strip().replace('<pad>', '').replace('</s>', '').strip(" ")
+
+        # Fix zero-width joiner issue.
+        out = out.replace("\u0dca \u0dbb", "\u0dca\u200d\u0dbb").replace("\u0dca \u0dba", "\u0dca\u200d\u0dba")
+        generated += out + "\n"
+    return generated
 
 
 class RequestBody(BaseModel):
@@ -43,7 +94,7 @@ def generate_simp(body: RequestBody):
         print("Output from /generate: " + str(out))
         is_busy = 0
         end_timer = timer()
-        print("Time taken: " + str(round(end_timer - start_timer, 4))+" s")
+        print("Time taken: " + str(round(end_timer - start_timer, 4)) + " s")
         return {"simplification": out}
     except Exception as e:
         is_busy = 0
