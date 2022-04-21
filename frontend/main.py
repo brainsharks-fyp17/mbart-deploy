80e3d1db8d2357a21b5b17789ab764fbaade2d19
rumesh <rumesh.m.s16@gmail.com>
2022-04-21 07:55:50 +0530

    changed frontend

--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,7 @@ import logging.config
 import os
 
 import gradio
+from text import description, article, examples
 from gradio.mix import Parallel
 import requests
 from dotenv import load_dotenv
@@ -12,6 +13,8 @@ load_dotenv()
 
 backend_port = os.environ['BACKEND_PORT']
 backend_host = os.environ['BACKEND_HOST']
+backend_port2 = os.environ['BACKEND_PORT2']
+backend_host2 = os.environ['BACKEND_HOST2']
 request_path = "generate"
 
 
@@ -21,10 +24,42 @@ def get_simplification(text):
     response = requests.post(url, json=data).json()
     return response["simplification"]
 
+def get_simplification2(text):
+    url = "http://" + backend_host2 + ":" + str(backend_port2) + "/" + request_path
+    data = {"text": text}
+    response = requests.post(url, json=data).json()
+    return response["simplification"]
 
 gradio.close_all()  # close already running instances
 text_input = gradio.inputs.Textbox(lines=10, placeholder="Type here", label="Complex Sentence")
 output = gradio.outputs.Textbox(type="str", label="Simplified")
 
-iface1 = gradio.Interface.load("huggingface/Rumesh/mbart-si-simp")
-Parallel(iface1).launch(share=False, server_port=5000, server_name="0.0.0.0")
+iface = gradio.Interface(
+    fn=get_simplification,
+    title="Simplification only",
+    description=description,
+    article=article,
+    inputs=[text_input],
+    outputs=output,
+    theme="dark-huggingface",
+    allow_flagging="never",
+    allow_screenshot=False,
+    analytics_enabled=True,
+    css="custom.css",
+    examples=examples
+)
+iface2 = gradio.Interface(
+    fn=get_simplification2,
+    title="Translation -> Simplification",
+    description=description,
+    article=article,
+    inputs=[text_input],
+    outputs=output,
+    theme="dark-huggingface",
+    allow_flagging="never",
+    allow_screenshot=False,
+    analytics_enabled=True,
+    css="custom.css",
+    examples=examples
+)
+Parallel(iface, iface2).launch(share=False, server_port=5000, server_name="0.0.0.0")
